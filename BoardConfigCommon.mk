4d10c0faa5665d89e92478edf5c14d8b7f73d175
Joe Wooster <jeebobs@btinternet.com>
2015-11-18 14:43:34 +0000

    fusion3: TEMP: Don't include custom RIL
    Change-Id: I0a85272334ae74681715b6729db5c06c279845a0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,8 +89,8 @@ GREEN_LED_PATH := /sys/class/leds/lm3533-green/brightness
 BLUE_LED_PATH := /sys/class/leds/lm3533-blue/brightness
 
 # RIL
-BOARD_PROVIDES_LIBRIL := true
-BOARD_RIL_CLASS := ../../../device/sony/fusion3-common/ril/
+#BOARD_PROVIDES_LIBRIL := true
+#BOARD_RIL_CLASS := ../../../device/sony/fusion3-common/ril/
 
 # Lights HAL
 TARGET_PROVIDES_FUSION3_LIBLIGHT := true

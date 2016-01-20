01f2b176596c35eeaca9ec08ed198acd12d81c74
Joe Wooster <jeebobs@btinternet.com>
2016-01-20 23:01:30 +0000

    fusion3: Use media codecs/profiles from flo

--- fusion3.mk
+++ fusion3.mk
@@ -155,6 +155,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(COMMON_PATH)/rootdir/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    $(COMMON_PATH)/rootdir/system/etc/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/rootdir/system/etc/media_profiles.xml:system/etc/media_profiles.xml
 
 # Lights wrapper
